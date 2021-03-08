0220037a9f05631b188599778832658d16423caa
Chethan D. Balakrishna <Chethan.Balakrishna@biberk.com>
2021-03-07 23:45:09 -0600

    Add project files.

---
+++ Acme.Common/OperationResult.cs
@@ -0,0 +1,23 @@
+﻿namespace Acme.Common
+{
+    /// <summary>
+    /// Provides a success flag and message 
+    /// useful as a method return type.
+    /// </summary>
+    public class OperationResult<T>
+    {
+        public OperationResult()
+        {
+        }
+
+        public OperationResult(T success, string message) : this()
+        {
+            this.Result = success;
+            this.Message = message;
+        }
+
+        public T Result { get; set; }
+        public string Message { get; set; }
+    }
+
+}
